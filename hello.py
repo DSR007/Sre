17de705fdb07ebf4bf34880596c8ea111d576535
DSR007 <34449152+DSR007@users.noreply.github.com>
2017-12-11 19:43:55 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello World')
+print('What is your name?')
+inName=input()
+print('Hello'+inName)
+print('Nice to meet you :)')
+
+print('How old are you?')
+inAge=input()
+print('Your age is '+str(int(inAge)))
